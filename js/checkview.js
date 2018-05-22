bfccea9c0196579d52bdab49df78ebf2ab7ed608
alex cotin <a.cotin96@yahoo.com>
2018-05-22 15:06:28 +0200

    added animation text

---
+++ js/checkview.js
@@ -0,0 +1,17 @@
+var $window = $(window);
+var $elem = $(".small");
+
+function isScrolledIntoView($elem, $window) {
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+$(document).on("scroll", function () {
+    if (isScrolledIntoView($elem, $window)) {
+        $elem.addClass("animation-text");
+    }
+    });
